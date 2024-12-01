be6acbfe11fc4a9d6def6fd83656c49b511b716e
Valentin Lilian <lilian@dynseo.com>
2024-12-01 20:43:49 +0100

    fix: display columns in BoardPage

--- src/ui/pages/Board/BoardPage.tsx
+++ src/ui/pages/Board/BoardPage.tsx
@@ -30,10 +30,31 @@ function BoardPage() {
         fetchBoard();
     }, [id])
 
+    const convertedColumns = board.columns.map((columnDto) => {
+        return {
+            id: columnDto.column.id, // ID de la colonne
+            title: columnDto.column.name, // Nom de la colonne
+            tasks: columnDto.cards.map(cardDto => cardDto.card.title) // Titres des cartes dans cette colonne
+        };
+    });
+
     return (
         <div>
             <Typography>{board.board.name}</Typography>
-            {/* <BoardListCarousel lists={convertedColumns}/> */}
+            <BoardListCarousel
+                lists={convertedColumns}
+                onMoveListLeft={() => {}}
+                onMoveListRight={() => {}}
+                onMoveTaskLeft={() => {}}
+                onMoveTaskRight={() => {}}
+                onDeleteList={() => {}}
+                onDeleteTask={() => {}}
+                onAddCard={() => {}}
+                onUpdateTask={() => {}}
+                onUpdateListTitle={() => {}}
+                onCancelAddList={() => {}}
+                onAddList={() => {}}
+            />
         </div>
     )
 }
