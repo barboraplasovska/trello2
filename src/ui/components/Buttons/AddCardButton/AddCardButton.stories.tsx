fe503ff8e9388a913515d043ae0c31c359a1419c
Barbora Plašovská <barbora.plasovska@epita.fr>
2024-12-04 10:54:39 +0100

    feat(storybook): tests for add card button

--- src/ui/components/Buttons/AddCardButton/AddCardButton.stories.tsx
+++ src/ui/components/Buttons/AddCardButton/AddCardButton.stories.tsx
@@ -1,8 +1,11 @@
+import { within, userEvent } from '@storybook/testing-library';
+import { expect } from '@storybook/test';
 import { fn } from '@storybook/test';
 import { AddCardButton } from './AddCardButton';
+import { StoryObj } from '@storybook/react/*';
 
 const ActionData = {
-    onClick: fn(),
+  onClick: fn(),
 };
 
 export default {
@@ -14,4 +17,26 @@ export default {
   },
 };
 
+type Story = StoryObj<typeof AddCardButton>;
+
 export const Default = {};
+
+export const Interactions: Story = {
+  args: {
+    onClick: ActionData.onClick,
+  },
+};
+
+Interactions.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  // Find and verify the button
+  const button = canvas.getByText('+ Add a card');
+  await expect(button).toBeInTheDocument();
+
+  // Simulate a click on the button
+  await userEvent.click(button);
+
+  // Ensure onClick callback was called
+  await expect(ActionData.onClick).toHaveBeenCalledTimes(1);
+};
