b8b2e32953bc9f5aba36f4ec43517afb5e90f4fb
Barbora Plašovská <barbora.plasovska@epita.fr>
2024-12-04 11:13:11 +0100

    feat(storybook): tests for list card

--- src/ui/components/Cards/ListCard/ListCard.stories.tsx
+++ src/ui/components/Cards/ListCard/ListCard.stories.tsx
@@ -1,5 +1,8 @@
 import { fn } from '@storybook/test';
 import { ListCard } from './ListCard';
+import { StoryObj } from '@storybook/react';
+import { within, userEvent } from '@storybook/testing-library';
+import { expect } from '@storybook/test';
 
 const ActionData = {
   onAddCard: fn(),
@@ -36,4 +39,36 @@ export default {
   },
 };
 
-export const Default = {};
+type Story = StoryObj<typeof ListCard>;
+
+export const Default: Story = {};
+
+export const Interactions: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    // Find the "Add Card" button and simulate a click
+    const addCardButton = await canvas.getByRole('button', { name: /add a card/i });
+    await userEvent.click(addCardButton);
+    await expect(ActionData.onAddCard).toHaveBeenCalledTimes(1);
+
+    // Simulate moving the list left
+    if (ActionData.moveListLeft) {
+      const moveLeftButton = await canvas.getByLabelText('Move list left');
+      await userEvent.click(moveLeftButton);
+      await expect(ActionData.moveListLeft).toHaveBeenCalledTimes(1);
+    }
+
+    // Simulate moving the list right
+    if (ActionData.moveListRight) {
+      const moveRightButton = await canvas.getByLabelText('Move list right');
+      await userEvent.click(moveRightButton);
+      await expect(ActionData.moveListRight).toHaveBeenCalledTimes(1);
+    }
+
+    // Simulate deleting the list
+    const deleteButton = await canvas.getByLabelText('Delete list');
+    await userEvent.click(deleteButton);
+    await expect(ActionData.onDelete).toHaveBeenCalledTimes(1);
+  },
+};
