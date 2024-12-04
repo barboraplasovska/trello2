3d98be903ad6a0cf816267ab92c54e339226bbbc
Barbora Plašovská <barbora.plasovska@epita.fr>
2024-12-04 10:56:07 +0100

    feat(storybook): tests for add list button

--- src/ui/components/Buttons/AddListButton/AddListButton.stories.tsx
+++ src/ui/components/Buttons/AddListButton/AddListButton.stories.tsx
@@ -1,8 +1,11 @@
+import { within, userEvent } from '@storybook/testing-library';
+import { expect } from '@storybook/test';
 import { fn } from '@storybook/test';
 import { AddListButton } from './AddListButton';
+import { StoryObj } from '@storybook/react/*';
 
 const ActionData = {
-    onClick: fn(),
+  onClick: fn(),
 };
 
 export default {
@@ -11,7 +14,29 @@ export default {
   tags: ['autodocs'],
   args: {
     ...ActionData,
-  }
+  },
 };
 
+type Story = StoryObj<typeof AddListButton>;
+
 export const Default = {};
+
+export const Interactions: Story = {
+  args: {
+    onClick: ActionData.onClick,
+  },
+};
+
+Interactions.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  // Find and verify the button
+  const button = canvas.getByText('+ Add another list');
+  await expect(button).toBeInTheDocument();
+
+  // Simulate a click on the button
+  await userEvent.click(button);
+
+  // Ensure onClick callback was called
+  await expect(ActionData.onClick).toHaveBeenCalledTimes(1);
+};
