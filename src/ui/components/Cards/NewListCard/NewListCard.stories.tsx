15738728badcf26fa06769d0184d6751b75dc7a0
Barbora Plašovská <barbora.plasovska@epita.fr>
2024-12-04 11:17:48 +0100

    feat(storybook): tests for new list card

--- src/ui/components/Cards/NewListCard/NewListCard.stories.tsx
+++ src/ui/components/Cards/NewListCard/NewListCard.stories.tsx
@@ -1,9 +1,12 @@
 import { fn } from '@storybook/test';
 import { NewListCard } from './NewListCard';
+import { StoryObj } from '@storybook/react';
+import { within, userEvent } from '@storybook/testing-library';
+import { expect } from '@storybook/test';
 
 const ActionData = {
-    onAddList: fn(),
-    onCancel: fn(),
+  onAddList: fn(),
+  onCancel: fn(),
 };
 
 export default {
@@ -11,8 +14,35 @@ export default {
   component: NewListCard,
   tags: ['autodocs'],
   args: {
-    ...ActionData
-  }
+    ...ActionData,
+  },
 };
 
-export const Default = {};
+type Story = StoryObj<typeof NewListCard>;
+
+export const Default: Story = {};
+
+export const Interactions: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    // Find the input field and type a title for the list
+    const inputField = await canvas.getByPlaceholderText('Enter list name');
+    await userEvent.type(inputField, 'New List');
+
+    // Find and click the "Add list" button
+    const addListButton = await canvas.getByRole('button', { name: /add list/i });
+    await userEvent.click(addListButton);
+
+    // Ensure the onAddList callback was called with the correct argument
+    await expect(ActionData.onAddList).toHaveBeenCalledWith('New List');
+    await expect(ActionData.onAddList).toHaveBeenCalledTimes(1);
+
+    // Find and click the "Cancel" button
+    const cancelButton = await canvas.getByLabelText('Cancel');
+    await userEvent.click(cancelButton);
+
+    // Ensure the onCancel callback was called once
+    await expect(ActionData.onCancel).toHaveBeenCalledTimes(1);
+  },
+};
